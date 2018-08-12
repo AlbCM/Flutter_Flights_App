331697e8dc1a40c7d9b665b948efe265384faa77
Albert <machado.albeiro@gmail.com>
2018-08-12 03:28:45 -0500

    Cards added

--- lib/main.dart
+++ lib/main.dart
@@ -88,80 +88,143 @@ class SearchScreen extends StatelessWidget {
             right: 10.0,
             top: 210.0,
             child: new Container(
-              width: 340.0,
-              height: 430.0,
-              decoration: new BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: 
-              new Column(
-                children: <Widget>[bodyTapBar()],
-              )  
-            )));
+                width: 340.0,
+                height: 430.0,
+                decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: new Column(
+                  children: <Widget>[bodyTapBar(), bodyCard()],
+                ))));
     return bContainer;
   }
 
   Widget bodyTapBar() {
     var bContent = Container(
-      padding: const EdgeInsets.only(top: 17.0 ),
-      child: Row(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Row(
+          children: <Widget>[
+            new Expanded(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 5.0, left: 16.0),
+                    decoration: BoxDecoration(
+                      border: new BorderDirectional(
+                          bottom: BorderSide(
+                              color: Colors.purple[600], width: 2.0)),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.flight,
+                          color: Colors.purple[600],
+                          size: 25.0,
+                        ),
+                        Text(" \$219",
+                            style: TextStyle(
+                                color: Colors.purple[600], fontSize: 18.0))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            new Expanded(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 5.0, left: 16.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.directions_railway,
+                          color: Colors.black26,
+                          size: 25.0,
+                        ),
+                        Text(" \$102",
+                            style: TextStyle(
+                                color: Colors.black26, fontSize: 18.0))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            new Expanded(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 5.0, left: 16.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.directions_car,
+                          color: Colors.black26,
+                          size: 25.0,
+                        ),
+                        Text(" \$74",
+                            style: TextStyle(
+                                color: Colors.black26, fontSize: 18.0))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+    return bContent;
+  }
+
+  Widget bodyCard() {
+    var card = Container(
+      padding: const EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20.0),
+      child: new Center(
+          child: Row(
         children: <Widget>[
           new Expanded(
-            child: Column(
+            child: Row(
               children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 5.0, left: 16.0),
-                  decoration: BoxDecoration(
-                    border: new BorderDirectional(bottom: BorderSide(color: Colors.purple[600], width: 2.0)),
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                    Icon(Icons.flight, color: Colors.purple[600], size: 25.0,),
-                    Text(" \$219", style: TextStyle(color: Colors.purple[600], fontSize: 18.0))
-                ],
+                Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black26,
                 ),
-                )
-              ],
-            ),
-          ),
-          new Expanded(
-          child: Column(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 5.0, left: 16.0),
-                  child:
-                  Row(
+                Column(
                   children: <Widget>[
-                  Icon(Icons.directions_railway, color: Colors.black26, size: 25.0,),
-                  Text(" \$102", style: TextStyle(color: Colors.black26, fontSize: 18.0))
-                ],
+                    Text(
+                      "6:00 am",
+                      style: TextStyle(color: Colors.black45, fontSize: 18.0),
+                    ),
+                    Text(
+                      "O' hare",
+                      style: TextStyle(color: Colors.black26),
+                    )
+                  ],
                 ),
-                )
-              ],
-            ),
-          ),
-          new Expanded(
-          child: Column(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 5.0, left: 16.0),
-                  child: Row(
+                Icon(Icons.flight, color: Colors.black26),
+                Column(
                   children: <Widget>[
-                  Icon(Icons.directions_car, color: Colors.black26, size: 25.0,),
-                  Text(" \$74", style: TextStyle(color: Colors.black26, fontSize: 18.0))
-                ],
-                ),),
+                    Text(
+                      "8:25 am",
+                      style: TextStyle(color: Colors.black45, fontSize: 18.0),
+                    ),
+                    Text(
+                      "Tacoma intl.",
+                      style: TextStyle(color: Colors.black26),
+                    )
+                  ],
+                ),
+                Text(
+                  "\$ 219",
+                  style: TextStyle(color: Colors.black45, fontSize: 16.0),
+                )
               ],
             ),
           )
         ],
-      )
+      )),
     );
-    return bContent;
-  }
-
-  Widget bodyCard(){
-    var card= Container();
 
     return card;
   }
